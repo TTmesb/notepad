39f2e9599882137f88654a25d32b0a1d1c9dd091
TTmesb <TTmesb@users.noreply.github.com>
2021-02-12 11:28:02 +0100

    menubar och layout

---
+++ src/guidesign.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class guidesign {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Notepad 2.0++");
+
+        frame.setContentPane(new guidesign().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        JMenu menu = new JMenu("Arkiv");
+        JMenuBar menubar = new JMenuBar();
+        JMenuItem spara = new JMenuItem("Spara din text bror");
+        menu.add(spara);
+        menubar.add(menu);
+        frame.setJMenuBar(menubar);
+
+
+    }
+    private JPanel panel1;
+    private JTextArea skrivyta;
+    private JButton saveButton;
+    private JButton button2;
+    private JButton copyTextButton;
+
+}
