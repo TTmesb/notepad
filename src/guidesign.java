439c8d958211760d40e974e987eccb964b09088c
TTmesb <emil.aberg@elev.ga.ntig.se>
2021-02-16 11:14:04 +0100

    donera knapp

--- src/guidesign.java
+++ src/guidesign.java
@@ -1,12 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 public class guidesign {
     public guidesign() {
+        JMenu menu = new JMenu("Arkiv");
+        menubar = new JMenuBar();
+        JMenuItem spara = new JMenuItem("Spara din text bror");
+        JMenuItem ny = new JMenuItem("Ny Textfil");
+        JMenuItem open = new JMenuItem("Öppna ");
+        donate = new JMenuItem("Donera");
+
+        menu.add(donate);
+        menu.add(open);
+        menu.add(ny);
+        menu.add(spara);
+        menubar.add(menu);
         clearButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -17,35 +30,37 @@ public class guidesign {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Clipboard kopiera = Toolkit.getDefaultToolkit().getSystemClipboard();
-                kopiera.setContents((Transferable) skrivyta, null);
+                kopiera.setContents(new StringSelection(skrivyta.getText()), null);
 
+            }
+        });
+        donate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    Desktop.getDesktop().browse(new URL("https://streamlabs.com/tmesb/tip").toURI());
+                } catch (Exception e) {}
             }
         });
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Notepad 2.0++");
-
-        frame.setContentPane(new guidesign().panel1);
+        guidesign gui = new guidesign();
+        frame.setContentPane(gui.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
 
-        JMenu menu = new JMenu("Arkiv");
-        JMenuBar menubar = new JMenuBar();
-        JMenuItem spara = new JMenuItem("Spara din text bror");
-        JMenuItem ny = new JMenuItem("Ny Textfil");
 
-        menu.add(ny);
-        menu.add(spara);
-        menubar.add(menu);
-        frame.setJMenuBar(menubar);
-
+        frame.setJMenuBar(gui.menubar);
+        frame.setPreferredSize(new Dimension(800,400));
 
     }
     private JPanel panel1;
     private JTextArea skrivyta;
     private JButton clearButton;
     private JButton copyTextButton;
-
+    private JMenuItem donate;
+    JMenuBar menubar;
 }
